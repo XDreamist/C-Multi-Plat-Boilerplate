d718df97e28b69021aa97f6725c4f92b31636c8b
XDreamist <umeshprakash123@outlook.com>
2025-01-16 11:11:27 +0000

    imgui init

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,9 @@
 #include <iostream>
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
-#include <GLFW/glfw3.h>
+
+#include "imguirunner.h"
 
 int main(int argc, char* argv[])
 {
-    if (!glfwInit())
-        return -1;
-
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui Example", NULL, NULL);
-    if (!window)
-        return -1;
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-
-    // Initialize OpenGL loader (e.g., GLAD)
-    // (Make sure you initialize OpenGL here before using ImGui)
-    
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    ImGui::StyleColorsDark();
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        ImGui::Text("Hello, Dear ImGui!");
-
-        ImGui::Render();
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-    }
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-    return 0;
+    ImGuiRunner* imRunner = new ImGuiRunner;
+    return imRunner->run();
 }
